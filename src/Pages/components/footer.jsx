51821c801f208d3169777284ccf9f02697151616
Hellodyonz <hellodyonz@gmail.com>
2023-12-05 17:47:22 +0700

    make footer components

--- src/Pages/components/footer.jsx
+++ src/Pages/components/footer.jsx
@@ -2,7 +2,16 @@
 
 function Footer(){
     return(
-        <div className="sosmed-footer">
+        <footer className="text-black">
+        <div className="container py-3">
+          <div className="row">
+            <div className="col-lg-5 col-md-12 mb-4 mb-md-0">
+              <img className="my-2" src="https://iili.io/JIFNa14.md.png" alt="" />
+              <h5 className="text-logo">
+                Mentor<span className="primary">ku</span>
+              </h5>
+              <p>Booking dan temukan mentor profesional dengan mudah.</p>
+              <div className="sosmed-footer">
                 <div className="sosmed-logo">
                   <img src="https://iili.io/JIFiSl1.png" alt="" />
                 </div>
@@ -15,7 +24,62 @@ function Footer(){
                 <div className="sosmed-logo">
                   <img src="https://iili.io/JIFieVV.png" alt="" />
                 </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-4 mb-4 mb-md-0">
+              <h5>Mentorku</h5>
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!">Karir</a>
+                </li>
+                <li>
+                  <a href="#!">Tentang Kami</a>
+                </li>
+                <li>
+                  <a href="#!">Partner</a>
+                </li>
+                <li>
+                  <a href="#!">Blog</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-4 mb-4 mb-md-0">
+              <h5>Layanan</h5>
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!">Mentoring</a>
+                </li>
+                <li>
+                  <a href="#!">Course</a>
+                </li>
+                <li>
+                  <a href="#!">Challenge</a>
+                </li>
+                <li>
+                  <a href="#!">Event</a>
+                </li>
+                <li>
+                  <a href="#!">Paket</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-3 col-md-4 mb-4 mb-md-0">
+              <h5>Bantuan dan Panduan</h5>
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!">Syarat dan Ketentuan</a>
+                </li>
+                <li>
+                  <a href="#!">Kebijakan Privasi</a>
+                </li>
+                <li>
+                  <a href="#!">Bantuan</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+      </footer>
     )
 }
 
